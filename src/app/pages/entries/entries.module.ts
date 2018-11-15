00077225321a5ff517446166d42971ea0c1e2492
fabioguelfi <fabioguelfiunix@gmail.com>
2018-11-14 23:04:08 -0200

    Melhorando o EntryForm (configurações iniciais)

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -5,16 +5,20 @@ import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import { IMaskModule } from "angular-imask";
 
 @NgModule({
-  declarations: [
-    EntryListComponent,
-    EntryFormComponent
-  ],
   imports: [
     CommonModule,
     EntriesRoutingModule,
     ReactiveFormsModule,
+    IMaskModule,
+    CalendarModule,
+  ],
+  declarations: [
+    EntryListComponent,
+    EntryFormComponent
   ]
 })
 export class EntriesModule { }
