0e30e31ba18e6dbf09b55f8705d5ac783fed3635
fabioguelfi <fabioguelfiunix@gmail.com>
2018-11-16 10:34:59 -0200

    Criando e integrando a classe 'BaseResourceModel'

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,7 +1,8 @@
-export class Category {
+import { BaseResourceModel } from "src/app/shared/models/base.resource.model";
+
+export class Category extends BaseResourceModel {
     constructor(
-        public id: number,
-        public name: string,
-        public description: string
-    ){}
+        public name?: string,
+        public description?: string
+    ){ super() }
 }
